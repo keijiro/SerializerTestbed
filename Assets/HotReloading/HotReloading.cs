2c2aee7e8765f898b9ddd372901db357bc511436
Keijiro Takahashi <keijiro@gmail.com>
2025-06-03 16:31:59 +0900

    hot reloading

--- Assets/HotReloading/HotReloading.cs
+++ Assets/HotReloading/HotReloading.cs
@@ -9,9 +9,17 @@ public sealed class HotReloading : MonoBehaviour
 
     (int x, int y) _t;
 
+    Object _obj;
+
+    static Object _obj2;
+
     void Start()
-      => (_x, _y, X, Y, _t) = (1, 2, 1, 2, (1, 2));
+    {
+        (_x, _y, X, Y, _t) = (1, 2, 1, 2, (1, 2));
+        _obj = ScriptableObject.CreateInstance<DestroyChecker>();
+        _obj2 = ScriptableObject.CreateInstance<DestroyChecker>();
+    }
 
     void Update()
-      => Debug.Log($"{_x}, {_y} | {X}, {Y} | {_t.x}, {_t.y}");
+      => Debug.Log($"{_x}, {_y} | {X}, {Y} | {_t.x}, {_t.y} | {_obj}, {_obj2}");
 }
