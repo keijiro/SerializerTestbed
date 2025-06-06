2dfe87f7bbe0a37ae15f8c2f557dcc95de9f7922
Keijiro Takahashi <keijiro@gmail.com>
2025-06-06 10:01:12 +0900

    asset bundle loading test

--- Assets/Bundles/BundleLoader.cs
+++ Assets/Bundles/BundleLoader.cs
@@ -2,25 +2,46 @@ using UnityEngine;
 
 public sealed class BundleLoader : MonoBehaviour
 {
+    AssetBundle LoadBundle1()
+    {
+        var path = Application.streamingAssetsPath + "/bundle1";
+        var bundle = AssetBundle.LoadFromFile(path);
+        var mat = bundle.LoadAsset<Material>("Assets/Bundles/Material2.mat");
+        GetComponent<MeshRenderer>().sharedMaterial = mat;
+        return bundle;
+    }
+
+    AssetBundle LoadBundle2()
+    {
+        var path = Application.streamingAssetsPath + "/bundle2";
+        return AssetBundle.LoadFromFile(path);
+    }
+
     async void Start()
     {
-        var path1 = Application.streamingAssetsPath + "/bundle1";
-        var path2 = Application.streamingAssetsPath + "/bundle2";
-
+        var bundle1 = LoadBundle1();
+        Debug.Log("Bundle 1 loaded");
         await Awaitable.WaitForSecondsAsync(1);
 
-        var bundle1 = AssetBundle.LoadFromFile(path1);
-
-        var mat = bundle1.LoadAsset<Material>("Assets/Bundles/Material2.mat");
-        GetComponent<MeshRenderer>().sharedMaterial = mat;
-
+        var bundle2 = LoadBundle2();
+        Debug.Log("Bundle 2 loaded");
         await Awaitable.WaitForSecondsAsync(1);
 
-        var bundle2 = AssetBundle.LoadFromFile(path2);
-
+        bundle1.Unload(true);
+        bundle2.Unload(true);
+        Debug.Log("Unloaded");
         await Awaitable.WaitForSecondsAsync(1);
 
-        mat = bundle1.LoadAsset<Material>("Assets/Bundles/Material2.mat");
-        GetComponent<MeshRenderer>().sharedMaterial = mat;
+        bundle2 = LoadBundle2();
+        Debug.Log("Bundle 2 loaded");
+        await Awaitable.WaitForSecondsAsync(1);
+
+        bundle1 = LoadBundle1();
+        Debug.Log("Bundle 1 loaded");
+        await Awaitable.WaitForSecondsAsync(1);
+
+        bundle1.Unload(true);
+        bundle2.Unload(true);
+        Debug.Log("Unloaded");
     }
 }
